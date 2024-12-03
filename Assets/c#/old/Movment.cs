61f2065376fc4689e84aecbd2b95ce974402eca7
sincereclout290 <caius.austin.658@accesscreative.ac.uk>
2024-12-03 16:19:47 +0000

    downoaded some sprites and changed the codes

---
+++ Assets/c#/old/Movment.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]         //Tell Unity to add theses components to the gameobject this code is attached to.
+[RequireComponent(typeof(BoxCollider2D))]       //We will still need to tweak some of the settings.
+public class RigidbodyMovement : MonoBehaviour
+{
+     public GameObject codeToStart; // Reference to the GameObject containing the code to start
+
+    Animator animator;
+    Rigidbody2D rb2d;
+    public float moveSpeed = 2.5f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            moveSpeed = 5f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+        {
+            moveSpeed = 2.5f;
+        }
+
+        float moveInputX = Input.GetAxisRaw("Horizontal"); // For horizontal movement (left/right)
+        float moveInputY = Input.GetAxisRaw("Vertical");   // For vertical movement (up/down)
+
+        animator.SetFloat("input x", moveInputX);
+        animator.SetFloat("input y", moveInputY);
+
+        // Normalise the vector so that we don't move faster when moving diagonally.
+        Vector2 moveDirection = new Vector2(moveInputX, moveInputY);
+        moveDirection.Normalize();
+
+        // Assign velocity directly to the Rigidbody
+        rb2d.velocity = moveDirection * moveSpeed;
+    }
+    
+}
+
---
+++ Assets/c#/old/PlayerShooting.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab; // Prefab of the bullet
+    public Transform firePointRotation;
+    public Transform bulletSpawnPoint;
+    public float bulletSpeed = 20f; // Speed of the bullet
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+         RotateBulletSpawnPointTowardsMouse();
+        // Check for the "Firel" input (left mouse button or spacebar by default)
+        if (Input.GetButtonDown ("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void RotateBulletSpawnPointTowardsMouse()
+    {
+        // Get the mouse position in screen space and convert it to world space
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        mousePosition.z = 0f; // Ensure z-axis is 0 for 2D space
+
+        // Calculate the direction from the player to the mouse
+        Vector2 direction = (mousePosition - firePointRotation.position).normalized;
+
+        // Calculate the angle to rotate the fire point (using Atan2 to get angle in degrees)
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+
+        // Apply the rotation to the fire point
+        firePointRotation.rotation = Quaternion. Euler (new Vector3(0, 0, angle));
+    }
+    void Shoot ()
+    {
+        // Instantiate the bullet at the fire point's position and rotation
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, firePointRotation.rotation);
+        // Get the Rigidbody2D component from the bullet
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        // Apply velocity to the bullet in the direction the fire point is facing
+        rb.velocity = firePointRotation.up * bulletSpeed;
+        Destroy(bullet, 5f);
+    }
+}
